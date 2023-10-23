1db9485b8c40c2979d439653a6541d645e23a972
Farooque Ansari <farooque.ansari@smarthub.ai>
2023-10-23 20:17:36 +0530

    Initial Commit

---
+++ BackEnd/FiftyThirtyTwenty/src/main/java/com/mdfa/FiftyThirtyTwenty/FiftyThirtyTwentyApplication.java
@@ -0,0 +1,13 @@
+package com.mdfa.FiftyThirtyTwenty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiftyThirtyTwentyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiftyThirtyTwentyApplication.class, args);
+	}
+
+}
